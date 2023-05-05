34343435e47aebd3bd2e8b9e5a3a8167997639a5
erik2344 <eblanco3@ucol.mx>
2023-05-05 00:05:49 -0600

    first commit

---
+++ lista.js
@@ -0,0 +1,9 @@
+const fecha = new Date(2003, 06, 20);
+const libro = {
+    titulo: "eleanor y parck",
+    autor: "rainbow rowel",
+    fecha: 2012
+}
+
+const lista = ["Erik", 19, false, fecha, libro]
+console.log(lista);
